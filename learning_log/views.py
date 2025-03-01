b0891bbdd6f7a0ff490928266484b31bf63d8c0c
MatSuzart <mathsuzart@outlook.com>
2025-02-28 22:19:30 -0300

    view-new-topic

--- learning_log/views.py
+++ learning_log/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Topic
+from .forms import TopicForm
 # Create your views here
 
 def index(request):
@@ -7,8 +8,25 @@ def index(request):
     return render(request,'learning_logs/index.html')
 
 def topics(request,topic_id):
+    
     '''Mostra todos os tópicos'''
     topic = Topic.objects.get(id=topic_id)
     entires = topic.entry_set.order_by('-date_added')
     context = {'topic':topic,'entires':entires}
     return render(request, 'learning_logs/topics.html',context)
+
+def new_topic(request):
+    '''Adiciona um tópico novo'''
+    if request.method !='POST':
+        #Nenhum dado enviado; cria um formulário em branco
+        form = TopicForm()
+    else:
+        #Dados POST enviados;processa os dados
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+#Exibe um formulário em bracno ou inválido
+    context = {'form':form}
+    return render(request,'learning_logs/new_topic.html',context) 
