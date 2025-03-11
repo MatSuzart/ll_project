dab78799b5c25979fd84919700d261989f07badb
MatSuzart <mathsuzart@outlook.com>
2025-03-10 23:31:10 -0300

    edit-forms

--- forms.py
+++ forms.py
@@ -1,9 +1,10 @@
 from django import forms
 
-from .models import Topic
+from .models import Topic,Entry
 
 class TopicForm(forms.ModelForm):
     class Meta:
+        model = Entry
         model = Topic
         fields = ['text']
-        labels = {'text': 'Texto'}
+        labels = {'text': forms.Textarea(attrs={'cols':80})}
